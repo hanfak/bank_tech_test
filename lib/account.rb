4761bc6d96de816d79c624749ca07d441a306ad4
Hanif Fakira <fakira.work@gmail.com>
2016-06-25 15:12:58 +0100

    refactoring

--- lib/account.rb
+++ lib/account.rb
@@ -1,29 +1,26 @@
 class Account
   attr_reader :balance, :history
 
-  HEADER = "date || credit || debit || balance\n"
-
   def initialize
     @balance = 0
     @history = []
   end
 
-  def credit(amount, date = Time.now.strftime("%m/%d/%y"))
+  def credit(amount, date = today)
     increase_balance(amount)
     add_transaction(amount, date, :deposit)
   end
 
-  def debit(amount, date = Time.now.strftime("%m/%d/%y"))
+  def debit(amount, date = today)
     reduce_balance(amount)
     add_transaction(amount, date, :withdraw)
   end
 
   def print_statement
-     HEADER  +  combined_transactions
+    statement_print
   end
 
   private
-
     def add_transaction(amount, date, type)
       transaction = {date: date, credit: currencify(amount), debit: nil, balance: currencify(@balance)} if type == :deposit
       transaction = {date: date, credit: nil, debit: currencify(amount), balance: currencify(@balance)} if type == :withdraw
@@ -38,10 +35,18 @@ class Account
       '%.2f' % amount
     end
 
+    def today
+      Time.now.strftime("%m/%d/%y")
+    end
+
     def combined_transactions
       @history.reverse.inject("") do |statement, transaction|
         statement << (stringfy(transaction) + "\n")
       end.strip
+    end
+
+    def statement_print
+      "date || credit || debit || balance\n"  +  combined_transactions
     end
 
     def reduce_balance(amount)
