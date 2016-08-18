8b465bafc51ad97cf321413e09da3a54d4b168e7
Hanif Fakira <fakira.work@gmail.com>
2016-08-18 17:58:30 +0100

    init rspec & feature test

---
+++ spec/features/feature_test_spec.rb
@@ -0,0 +1,16 @@
+describe 'feature test' do
+  header = "date || credit || debit || balance\n"
+  first_deposit = "10/01/12 || 1000.00 ||  || 1000.00"
+  second_deposit = "13/01/12 || 2000.00 ||  || 3000.00\n"
+  first_withdrawal = "14/01/12 ||  || 500.00 || 2500.00\n"
+
+  output = header + first_withdrawal + second_deposit + first_deposit
+
+  it 'prints multiple transactions' do
+    account = Account.new
+    account.credit(1000, '10/01/12')
+    account.credit(2000, '13/01/12')
+    account.debit(500, '14/01/12')
+    expect(account.print_statement).to eq output
+  end
+end
